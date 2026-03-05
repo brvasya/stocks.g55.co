39d9dff4352be687b3516c99bf8d7c7f5e3b04b1
brvasya <crazygames888@gmail.com>
2026-03-05 20:27:45 +0300

    add

--- menu.php
+++ menu.php
@@ -1,2 +1,3 @@
 <a class="item" href="stocks.php?s1=MSFT&s2=MA&p1=4&p2=3">MSFT - MA</a>
+<a class="item" href="stocks.php?s1=MSFT&s2=V&p1=4&p2=5">MSFT - V</a>
 <a class="item" href="stocks.php?s1=V&s2=MA&p1=5&p2=3">V - MA</a>
