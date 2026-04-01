ccaaf8defe72808ff6608dd04703351b2bd907c4
brvasya <crazygames888@gmail.com>
2026-04-01 16:50:13 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,3 +1,3 @@
-<a class="item" href="stocks.php?s1=MSFT&s2=MA&p1=4&p2=3">MSFT - MA</a>
-<a class="item" href="stocks.php?s1=MSFT&s2=V&p1=4&p2=5">MSFT - V</a>
+<a class="item" href="stocks.php?s1=GOOGL&s2=MA&p1=10&p2=3">GOOGL - MA</a>
+<a class="item" href="stocks.php?s1=GOOGL&s2=V&p1=10&p2=5">GOOGL - V</a>
 <a class="item" href="stocks.php?s1=V&s2=MA&p1=5&p2=3">V - MA</a>
