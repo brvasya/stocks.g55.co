88f323234232499b605c0784d0eaed57e3d32c72
brvasya <crazygames888@gmail.com>
2026-06-11 00:04:09 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,3 +1,3 @@
-<a class="item" href="stocks.php?s1=GOOGL&s2=MA&p1=5&p2=3">GOOGL - MA</a>
-<a class="item" href="stocks.php?s1=GOOGL&s2=V&p1=5&p2=5">GOOGL - V</a>
+<a class="item" href="stocks.php?s1=AMZN&s2=MA&p1=6&p2=3">AMZN - MA</a>
+<a class="item" href="stocks.php?s1=AMZN&s2=V&p1=6&p2=5">AMZN - V</a>
 <a class="item" href="stocks.php?s1=V&s2=MA&p1=5&p2=3">V - MA</a>
